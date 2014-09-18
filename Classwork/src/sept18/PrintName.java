a0bdf608306fb212134882c77531f0cb4627a345
Andrew Violette <violetteavi@gmail.com>
2014-09-18 08:25:20 -0700

    Classwork for Sept 18

---
+++ Classwork/src/sept18/PrintName.java
@@ -0,0 +1,22 @@
+package sept18;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		PrintName.printFirst();
+		System.out.print(" ");
+		PrintName.printLast();
+	}
+	
+	public static void printFirst() {
+		System.out.print("Andrew");
+	}
+	
+	public static void printMiddle() {
+		System.out.print("Mark");
+	}
+	
+	public static void printLast() {
+		System.out.print("Violette");
+	}
+}
