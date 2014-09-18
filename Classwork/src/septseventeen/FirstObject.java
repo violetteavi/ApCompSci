3342848af16ac733f43a36aa33641d8a36e52e0b
Andrew Violette <violetteavi@gmail.com>
2014-09-18 08:20:06 -0700

    Classwork for APCompSci-- Sept 16, 17

---
+++ Classwork/src/septseventeen/FirstObject.java
@@ -0,0 +1,20 @@
+package septseventeen;
+
+public class FirstObject {
+
+	public static void main(String[] args) {
+		FirstObject.printName();
+		FirstObject.goEagles();
+	}
+	
+	private static void printName() {
+		System.out.println("My name is Andrew");
+		System.out.println("and I love Java!!");
+	}
+	
+	private static void goEagles() {
+		System.out.print("Go ");
+		System.out.print("Issaquah ");
+		System.out.println("Eagles!");
+	}
+}
---
+++ Classwork/src/septsixteen/FirstProgram.java
@@ -0,0 +1,9 @@
+package septsixteen;
+
+public class FirstProgram {
+
+	public static void main (String[] args) {
+		System.out.println("Hello, world!");
+	}
+	
+}
