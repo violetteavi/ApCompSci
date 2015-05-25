9504dbedd6fd1a1798df818c598ba7883975b9c6
violetteavi <violettavi@gmail.com>
2015-05-25 12:12:39 -0700

    Test commit

--- Classwork/classwork/jan6/TriangleTest.java
+++ Classwork/classwork/jan6/TriangleTest.java
@@ -14,6 +14,7 @@ public class TriangleTest {
 		Point p2 = new Point(2, 2);
 		Point p3 = new Point(2, 0);
 		assertEquals((2+2+Math.sqrt(2)*2), Triangle.perimeter(new Triangle(p1,p2,p3)), 0.0000001);
+		System.out.println("I am a walrus!");
 	}
 	
 }
